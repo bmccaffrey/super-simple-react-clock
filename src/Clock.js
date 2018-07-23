e4cbde8492c85fdd3d2feacf2f001ee31aef4392
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-23 17:52:45 -0500

    convert functional component to class

--- src/Clock.js
+++ src/Clock.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export default function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+export default class Clock extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
